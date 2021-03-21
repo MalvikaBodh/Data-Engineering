1f500f1a9b9ef1a171abeaba4b6df9560f894e3c
Malvika Bodh <malvikabodh@gmail.com>
2021-03-21 16:45:09 -0500

    Project Files

---
+++ Project 1: Data Modeling with Postgres/sql_queries.py
@@ -0,0 +1,96 @@
+# DROP TABLES
+songplay_table_drop = "DROP TABLE IF EXISTS songplay;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users  
+        (user_id int PRIMARY KEY, 
+        first_name varchar, 
+        last_name varchar, 
+        gender varchar, 
+        level varchar NOT NULL);""")
+       
+
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs  
+        (song_id varchar PRIMARY KEY, 
+        title varchar NOT NULL, 
+        artist_id varchar NOT NULL,  
+        year int NOT NULL, 
+        duration float NOT NULL);""")
+
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists 
+        (artist_id varchar PRIMARY KEY, 
+        name varchar NOT NULL, 
+        location varchar NOT NULL, 
+        latitude  NUMERIC (9, 5), 
+        longitude  NUMERIC (9, 5));""")
+
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time 
+        (start_time timestamp PRIMARY KEY, 
+        hour int NOT NULL, 
+        day int NOT NULL,  
+        week int NOT NULL, 
+        month int NOT NULL,  
+        year int NOT NULL,  
+        weekday int NOT NULL);""")
+
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplay 
+        (songplay_id SERIAL PRIMARY KEY, 
+        start_time TIMESTAMP , 
+        user_id int NOT NULL , 
+        level varchar NOT NULL,
+        song_id varchar, 
+        artist_id varchar, 
+        session_id int NOT NULL, 
+        location varchar, 
+        user_agent text,
+        CONSTRAINT songplay_fk_user
+             foreign key (user_id) 
+             REFERENCES users (user_id),
+        CONSTRAINT songplay_fk_songs
+             foreign key (song_id) 
+             REFERENCES songs (song_id),
+        CONSTRAINT songplay_fk_artist
+             foreign key (artist_id) 
+             REFERENCES artists (artist_id),
+        CONSTRAINT songplay_fk_time
+             foreign key (start_time) 
+             REFERENCES time (start_time)); """)
+
+
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""INSERT INTO songplay 
+                    (start_time,user_id,level,song_id, artist_id, session_id, location, user_agent) VALUES 
+                    ( %s,%s,%s,%s,%s,%s,%s,%s);""")
+
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s,%s,%s,%s,%s)
+                    ON CONFLICT (user_id) DO UPDATE
+                    SET level =excluded.level;""")
+
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s,%s,%s,%s,%s)
+                    ON CONFLICT (song_id) DO NOTHING;""")
+
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s,%s,%s,%s,%s)
+                    ON CONFLICT (artist_id) DO NOTHING;""")
+
+
+time_table_insert = ("""INSERT INTO time (start_time,hour,day,week,month,year,weekday) VALUES (%s,%s,%s,%s,%s,%s,%s)
+                    ON CONFLICT (start_time) DO NOTHING;""")
+
+# FIND SONGS : # get songid and artistid from song and artist tables
+
+song_select = (""" SELECT songs.song_id, artists.artist_id 
+                FROM songs  JOIN artists ON songs.artist_id = artists.artist_id
+                   WHERE songs.title = %s
+                    AND artists.name = %s
+                    AND songs.duration = %s""")
+
+# QUERY LISTS
+
+create_table_queries = [ user_table_create , song_table_create, artist_table_create, time_table_create,songplay_table_create]
+drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop,songplay_table_drop]
